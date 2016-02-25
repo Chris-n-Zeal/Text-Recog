dc727b21350882e64aa2d4135dcd3976c0c200ca
zealyim <swdreamwa@hotmail.com>
2016-02-24 16:19:59 -0800

    Compress image before ASCII-lize it (not finish)
    Renamed some variables.
    Added comments.
    In the function imgToArr(img):
    *added if function to decide whether or not the image needs to be
    shrink/compress

--- ImageProcess.py
+++ ImageProcess.py
@@ -1,8 +1,10 @@
 #### ImageProcess.py
-# Takes a jpg image and prints a version of the image with white 
-# pixels as space and non-white pixels as characters.
+# Takes a jpg image in white background and prints a version of the image 
+# with white pixels as space and non-white pixels as characters.
 #
 # Authors: Chris Horan, Zeal Yim
+# Created: Feb 23 2016
+# Last modified: Feb 24 2016 by Zeal
 ####
 
 from __future__ import print_function
@@ -10,30 +12,55 @@ from PIL import Image
 
 THRESHOLD_MARGIN = 0.95 #Filters out near-white pixels
 WHITE = 765 #RGB
+MAX_HEIGHT_OUTPUT = 200
+MAX_WIDTH_OUTPUT = 200
 
-def imgToArr(pic):
+def imgToArr(img):
 	nearWhite = WHITE * THRESHOLD_MARGIN
+	pixelsArr = []
+	imgWidth, imgHeight = img.size
 
-	pixels = []
-
-	wid, hei = img.size
-
-	i = 0
-	tmp = []
-
-	for pixel in iter(img.getdata()):
-		r,g,b = pixel
-		pixelVal = r + g + b
-		if pixelVal < nearWhite:
-			tmp.append("M ")
-		else:
-			tmp.append("  ")
-		i += 1
-		if i == wid:
-			pixels.append(tmp)
-			tmp = []
-			i = 0
-	return pixels
+	if imgWidth > MAX_WIDTH_OUTPUT or imgHeight > MAX_HEIGHT_OUTPUT: #check if img needs to be compressed
+		imgArr = []
+		compressedImgArr = []
+		tmp = []
+		pixelCounter = 0
+		#convert img's pixel into a 2D array
+		for pixel in iter(img.getdata()):
+			tmp.append(pixel)
+			pixelCounter += 1
+			if pixelCounter >= imgWidth:
+				imgArr.append(tmp)
+				tmp = [] #still looking at how python handles reassignment regarding memory management
+				pixelCounter = 0
+		#calculate the ratio the img needs to be compressed at
+		compressRatio = int(round(max(imgWidth/MAX_HEIGHT_OUTPUT, imgHeight/MAX_HEIGHT_OUTPUT)))
+		#compress imgArr and put in to compressedImgArr for later process
+		topLeftPixelRow = 0
+		topLeftPixelCol = 0
+		btnRightPixelRow = topLeftPixelRow + compressRatio
+		btnRightPixelCol = topLeftPixelCol + compressRatio
+		nextBtnRightPixelCol = btnRightPixelCol
+		averageRGB = 0,0,0
+		while nextBtnRightPixelCol <= imgHeight:
+			averageRGB = ()
+*************************************************************************************************************
+	else: #img does not need to be compressed
+		tmp = []
+		pixelCounter = 0
+		for pixel in iter(img.getdata()):
+			r,g,b = pixel
+			pixelVal = r + g + b
+			if pixelVal < nearWhite:
+				tmp.append("M ")
+			else:
+				tmp.append("  ")
+			pixelCounter += 1
+			if pixelCounter == imgWidth:
+				pixelsArr.append(tmp)
+				tmp = []
+				pixelCounter = 0
+	return pixelsArr
 
 def printImgArr(imgArr):
 	for i in range(len(imgArr)):
@@ -54,13 +81,13 @@ def shrinkImgArr(imgArr):
 		i += 2
 		k += 1
 	return newArr
+def average(list):
+	return sum(list)/list.len
 
-
-
-
+#main
 f = open('asciiArt.txt', 'w')
 
-img = Image.open("cin.jpg")
+img = Image.open("logo.jpg")
 
 pixArr = imgToArr(img)
 
